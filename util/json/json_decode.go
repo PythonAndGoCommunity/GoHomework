ce5848ad6e74954c109c17a12e1131f2ad0a86cf
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-11-20 15:00:12 +0300

    Added comments for json util & marshal indent and fixedjson decode func signature

--- util/json/json_decode.go
+++ util/json/json_decode.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 )
 
+// UnpackFromJSON | Receives json bytes and returns map pointer.
 func UnpackFromJSON(b []byte) *map[string]interface{}{
 	var m map[string]interface{}
 	err := json.Unmarshal(b, &m)
--- util/json/json_encode.go
+++ util/json/json_encode.go
@@ -4,17 +4,19 @@ import (
 	"encoding/json"
 )
 
+// PackToJSON | Receives string key and interface value and returns json bytes.
 func PackToJSON(key string, value interface{}) []byte{
 	m := map[string]interface{}{key : value}
-	b, err := json.Marshal(m)
+	b, err := json.MarshalIndent(m,""," ")
 	if err != nil{
 		panic(err)
 	}
 	return b
 }
 
-func PackMapToJson(m map[string]interface{}) []byte{
-	b, err := json.Marshal(m)
+// PackMapToJSON | Receives map and returns json bytes.
+func PackMapToJSON(m map[string]interface{}) []byte{
+	b, err := json.MarshalIndent(m,""," ")
 	if err != nil{
 		panic(err)
 	}
