0661dc00b9731279065bdc0d50dc7f90a68b30c8
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-11-22 16:58:41 +0300

    Added logging to inmemory package & add behaviour if db file not exist

--- server/storage/inmemory/inmemory_service.go
+++ server/storage/inmemory/inmemory_service.go
@@ -1,8 +1,10 @@
 package inmemory
 
 import (
+	"os"
 	"NonRelDB/server/util/json"
 	"NonRelDB/server/util/file"
+	"NonRelDB/server/log"
 )
 
 // Storage | Global variable for kv storage.
@@ -11,11 +13,30 @@ var Storage *map[string]string
 // InitDBInMemory | Init kv db in memory.
 func InitDBInMemory(){
 	s := make(map[string]string)
-	Storage = &s
+	Storage = &s	
+	log.Info.Println("DB successfully created in-memory")
 }
 
 // InitDBFromStorage | Receives filename and load its content to inmemory storage.
 func InitDBFromStorage(filename string){
+	log.Info.Println("[" + filename+"]")
+	log.Info.Println(len(filename))
+
+	_, err := os.Stat(filename)
+
+	if os.IsNotExist(err) {
+		log.Warning.Println(err.Error())
+		log.Warning.Printf("Storage doesnt exist. Will be created new with name %s", filename)
+
+		f, err := os.Create(filename)
+		
+		if err != nil {
+			log.Error.Panicln(err.Error())
+		}
+		log.Info.Printf("DB successfully initialized from %s", filename)
+		f.Close()
+	}
+
 	j := file.OpenAndReadString(filename)
 	jb := []byte(j)
 	Storage = json.UnpackFromJSON(jb)
@@ -25,5 +46,7 @@ func InitDBFromStorage(filename string){
 func SaveDBToStorage(filename string){
 	jb := json.PackMapToJSON(*Storage)
 	j := string(jb)
+
 	file.CreateAndWriteString(filename, j)
+	log.Info.Printf("DB successfully saved to %s", filename)
 }
