f3f9798e1b94a4eb8d8a2051b0d048ce867672bc
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-11-22 16:50:43 +0300

    Added logging to json package & fixed nil assignment map panic in json_decode.go

--- server/util/json/json_decode.go
+++ server/util/json/json_decode.go
@@ -2,14 +2,16 @@ package json
 
 import (
 	"encoding/json"
+	"NonRelDB/server/log"
 )
 
 // UnpackFromJSON | Receives json bytes and returns map pointer.
 func UnpackFromJSON(b []byte) *map[string]string{
-	var m map[string]string
+	m := make(map[string]string)
 	err := json.Unmarshal(b, &m)
 	if err != nil{
-		panic(err)
+		log.Warning.Println(err.Error())
+		log.Warning.Println("No bytes. Will be returned zero map")
 	}
 	return &m
 }
--- server/util/json/json_encode.go
+++ server/util/json/json_encode.go
@@ -2,6 +2,7 @@ package json
 
 import (
 	"encoding/json"
+	"NonRelDB/server/log"
 )
 
 // PackToJSON | Receives string key and interface value and returns json bytes.
@@ -9,7 +10,7 @@ func PackToJSON(key string, value string) []byte{
 	m := map[string]string{key : value}
 	b, err := json.MarshalIndent(m,""," ")
 	if err != nil{
-		panic(err)
+		log.Error.Println(err.Error())
 	}
 	return b
 }
@@ -18,7 +19,7 @@ func PackToJSON(key string, value string) []byte{
 func PackMapToJSON(m map[string]string) []byte{
 	b, err := json.MarshalIndent(m,""," ")
 	if err != nil{
-		panic(err)
+		log.Error.Println(err.Error())
 	}
 	return b
 }
