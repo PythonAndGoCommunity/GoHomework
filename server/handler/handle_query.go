92ec5cdefaa8778406fde39a7fec42f6dcd8f090
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-03 07:14:11 +0300

    Server's query handler reworked using regexp

--- server/handler/handle_query.go
+++ server/handler/handle_query.go
@@ -1,24 +1,63 @@
 package handler
 
 import (
+	"fmt"
 	"strings"
+	"regexp"
 	"NonRelDB/server/storage/inmemory"
 )
 
+var keyGetDelReg *regexp.Regexp
+var keySetReg *regexp.Regexp
+var valueReg *regexp.Regexp
+var getReg *regexp.Regexp
+var setReg *regexp.Regexp
+var delReg *regexp.Regexp
+var keysReg *regexp.Regexp
+
+func init(){
+	
+	keyGetDelReg = regexp.MustCompile("\\s(.*)$")
+
+	keySetReg = regexp.MustCompile("\\s(.*)\\s")
+
+	valueReg = regexp.MustCompile("\"(.*)\"$")
+	
+	getReg = regexp.MustCompile("^get\\s(.*)$")
+
+	setReg = regexp.MustCompile("^set\\s(.*)\\s\"(.*)\"$")
+
+	delReg = regexp.MustCompile("^del\\s(.*)$")
+
+	keysReg = regexp.MustCompile("^keys\\s\"(.*?)\"$")
+}
+
 func HandleQuery(q string) string {
-	p := strings.Split(q, " ")
-	if len(p) == 2 {
-		if strings.ToLower(p[0]) == "get" {
-			return inmemory.GetStorage().Get(p[1])
-		} else if strings.ToLower(p[0]) == "del" {
-			return inmemory.GetStorage().Del(p[1])
-		} else if strings.ToLower(p[0]) == "keys" {
-			return inmemory.GetStorage().Keys(p[1])
-		}
-	} else if len(p) >= 3 {
-		if strings.ToLower(p[0]) == "set" {
-			return inmemory.GetStorage().Set(p[1], strings.Join(p[2:]," "))
-		}
+	fmt.Println("[" + q + "]")
+
+	if getReg.MatchString(q) {
+		key := strings.Trim(keyGetDelReg.FindString(q), " ")
+		fmt.Println("[" + key + "]")
+		return inmemory.GetStorage().Get(key)
+
+	} else if setReg.MatchString(q) {
+		key := strings.Trim(keySetReg.FindString(q), " ")
+		value := strings.Trim(valueReg.FindString(q), "\"")
+		fmt.Println("[" + key + "]")
+		fmt.Println("[" + value + "]")
+		return inmemory.GetStorage().Set(key, value )
+
+	} else if delReg.MatchString(q) {
+		key := strings.Trim(keyGetDelReg.FindString(q), " ")
+		fmt.Println("[" + key + "]")
+		return inmemory.GetStorage().Del(key)
+		
+	} else if keysReg.MatchString(q) {
+		reg := strings.Trim(valueReg.FindString(q),"\"")
+		fmt.Println("[" + reg + "]")
+		return inmemory.GetStorage().Keys(reg)
+
 	}
+
 	return "Undefined query"
 }
