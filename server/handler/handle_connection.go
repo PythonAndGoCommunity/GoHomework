0a71b084e7bd23b952f4cb7be6967f87b04a5ad3
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-08 04:04:39 +0300

    Moved HandleRequest func body to main to avoid pointless rune switch & return

--- server/handler/handle_connection.go
+++ server/handler/handle_connection.go
@@ -12,53 +12,6 @@ import (
 	"NonRelDB/util/regex"
 )
 
-// HandleRequest handling request from client.
-func HandleRequest(req string, c net.Conn) rune{
-	if regex.QueryReg.MatchString(req){
-		resp:= HandleQuery(req)
-		SendResponse(resp, c)
-		return 'c'
-
-	} else if regex.ExitReg.MatchString(req){
-		log.Info.Printf("%s disconnected from server", c.RemoteAddr().String())
-		return 'r'
-
-	} else if regex.DumpReg.MatchString(req){
-		dbDump := string(json.PackMapToJSON((*inmemory.GetStorage().GetMap())))
-		fmt.Fprintf(c, dbDump + "\n")
-		log.Info.Printf("Sent db dump to %s", c.RemoteAddr().String())
-		return 'r'
-
-	} else if regex.TopicReg.MatchString(req){
-		reqParts := strings.Split(req, " ")
-
-		if len(reqParts) == 2 {
-			if strings.ToLower(reqParts[0]) == "subscribe"{
-				topic.Subscribe(reqParts[1], c)
-				return 'c'
-
-			} else if strings.ToLower(reqParts[0]) == "unsubscribe"{
-				topic.Unsubscribe(reqParts[1], c)
-				return 'r'
-
-			}
-
-		} else if len(reqParts) >= 3{
-			if strings.ToLower(reqParts[0]) == "publish"{
-				msg := regex.DoubleQuoteReg.FindString(req)
-				topic.Publish(reqParts[1], msg)
-				return 'c'
-			}
-
-		}
-
-	} else {
-		SendResponse("Bad request", c)
-	}
-
-	return 'c'
-}
-
 // SendResponse sends response to specified connection.
 func SendResponse(resp string, c net.Conn){
 	fmt.Fprintf(c, resp + "\n")
@@ -82,9 +35,43 @@ func HandleConnection(c net.Conn){
 
 		log.Info.Printf("Received request from %s -> %s", c.RemoteAddr().String(), req)
 
-		switch handleCtx := HandleRequest(req, c); handleCtx{
-			case 'r': return
-			case 'c': continue
+		if regex.QueryReg.MatchString(req){
+			resp:= HandleQuery(req)
+			SendResponse(resp, c)
+	
+		} else if regex.ExitReg.MatchString(req){
+			log.Info.Printf("%s disconnected from server", c.RemoteAddr().String())
+			return
+	
+		} else if regex.DumpReg.MatchString(req){
+			dbDump := string(json.PackMapToJSON((*inmemory.GetStorage().GetMap())))
+			fmt.Fprintf(c, dbDump + "\n")
+			log.Info.Printf("Sent db dump to %s", c.RemoteAddr().String())
+			return
+	
+		} else if regex.TopicReg.MatchString(req){
+			reqParts := strings.Split(req, " ")
+	
+			if len(reqParts) == 2 {
+				if strings.ToLower(reqParts[0]) == "subscribe"{
+					topic.Subscribe(reqParts[1], c)
+	
+				} else if strings.ToLower(reqParts[0]) == "unsubscribe"{
+					topic.Unsubscribe(reqParts[1], c)
+					return
+	
+				}
+	
+			} else if len(reqParts) >= 3{
+				if strings.ToLower(reqParts[0]) == "publish"{
+					msg := regex.DoubleQuoteReg.FindString(req)
+					topic.Publish(reqParts[1], msg)
+				}
+	
+			}
+	
+		} else {
+			SendResponse("Bad request", c)
 		}
 	}
 }
