d133685263384cb37cc5347941db81005777c845
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-10 22:19:36 +0300

    Added unit tests for util/collection package

---
+++ util/collection/slice_util_test.go
@@ -0,0 +1,38 @@
+package collection
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"net"
+)
+
+type testConn struct{
+	net.Conn
+	id int
+}
+
+func TestConIndex__Contains__Success(t *testing.T){
+	testSlice := make([]net.Conn, 10, 10)
+	
+	neededConn := testConn{id: 123}
+
+	for i := 0; i < len(testSlice); i++ {
+		testSlice[i] = testConn{id : i}
+	}
+
+	testSlice[4] = neededConn
+
+	assert.Equal(t, 4, ConnIndex(testSlice, neededConn))
+}
+
+func TestConIndex__DoesntContain__Fail(t *testing.T){
+	testSlice := make([]net.Conn, 10, 10)
+	
+	neededConn := testConn{id: 123}
+
+	for i := 0; i < len(testSlice); i++ {
+		testSlice[i] = testConn{id : i}
+	}
+
+	assert.Equal(t, -1, ConnIndex(testSlice, neededConn))
+}
