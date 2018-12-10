130f25a14a9bb83d13f953032a5a0fa3ba61907b
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-10 20:06:44 +0300

    Removed Index func from util because it unused

--- util/collection/slice_util.go
+++ util/collection/slice_util.go
@@ -4,16 +4,6 @@ import (
 	"net"
 )
 
-// Index returns index of neccessary element in interface{} slice if found, otherwise -1.
-func Index(slice []interface{}, value interface{}) int{
-	for index, slice_value := range slice {
-		if slice_value == value {
-			return index
-		}
-	}
-	return -1
-}
-
 // ConnIndex returns index of neccessary element in net.Conn slice if found, otherwise -1.
 func ConnIndex(slice []net.Conn, value net.Conn) int{
 	for index, slice_value := range slice {
