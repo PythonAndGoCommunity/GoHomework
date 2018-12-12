90e41395c3ead5d981ef6c6eb4cf7be96a362810
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-12 11:36:39 +0300

    Added unit tests for storage/inmemory package

---
+++ server/storage/inmemory/inmemory_service_test.go
@@ -0,0 +1,78 @@
+package inmemory
+
+import (
+	"NonRelDB/util/json"
+	"os"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"NonRelDB/util/sync"
+	"NonRelDB/util/file"
+)
+
+func TestGetStorage__Empty__Fail(t *testing.T){
+	var syncMap *sync.Map
+	assert.Equal(t, syncMap, GetStorage())
+}
+
+func TestGetStorage__Existing__Success(t *testing.T){
+	testMap := map[string]string{"1" : "one", "2" : "two", "3" : "three"}
+	syncMap := sync.Map{}
+	syncMap.SetMap(&testMap)
+	storage = &syncMap
+	assert.Equal(t, &syncMap, GetStorage())
+}
+
+func TestSetStorage__NonEmpty__Success(t *testing.T){
+	testMap := map[string]string{"1" : "one", "2" : "two", "3" : "three"}
+	syncMap := sync.Map{}
+	syncMap.SetMap(&testMap)
+	SetStorage(&syncMap)
+	assert.Equal(t, &syncMap, GetStorage())
+}
+
+func TestSetStorage__Empty__Fail(t *testing.T){
+	emptyMap := sync.Map{}
+	SetStorage(&emptyMap)
+	assert.Equal(t, &emptyMap, GetStorage())
+}
+
+func TestInitDBInMemory__Success(t *testing.T){
+	assert.NotPanics(t, func(){
+		InitDBInMemory()
+	})
+}
+
+func TestInitDBFromStorage__Existing__Success(t *testing.T){
+	file.CreateAndWriteString("test.json","{\"123\":\"123\"}")
+	InitDBFromStorage("test.json")
+	testMap := map[string]string{"123":"123"}
+	assert.Equal(t, testMap, *(GetStorage().GetMap()))
+
+	os.Remove("test.json")
+}
+
+func TestInitDBFromStorage__NotExisting__Success(t *testing.T){
+	InitDBFromStorage("test.json")
+	testMap := map[string]string{}
+	assert.Equal(t, testMap, *(GetStorage().GetMap()))
+
+	os.Remove("test.json")
+}
+
+func TestRestoreDBFromDump__NonEmpty__Success(t *testing.T){
+	testMap := map[string]string{"1" : "one", "2" : "two", "3" : "three"}
+	RestoreDBFromDump(json.PackMapToJSON(testMap))
+	assert.Equal(t, testMap, *(GetStorage().GetMap()))
+}
+
+func TestSaveDBToStorage__NonEmpty__Success(t *testing.T){
+	testMap := map[string]string{"1" : "one", "2" : "two", "3" : "three"}
+	syncMap := sync.Map{}
+	syncMap.SetMap(&testMap)
+	SetStorage(&syncMap)
+	SaveDBToStorage("test.json")
+	assert.Equal(t, "{\"1\":\"one\",\"2\":\"two\",\"3\":\"three\"}", file.OpenAndReadString("test.json"))
+
+	os.Remove("test.json")
+}
+
