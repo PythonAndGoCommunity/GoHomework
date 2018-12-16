9bc932407b85cacfd7b1a07fa3f6c6044378d8a8
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-17 01:12:56 +0300

    Changed ip flag to host flag

--- server/server.go
+++ server/server.go
@@ -11,13 +11,14 @@ import (
 	"syscall"
 )
 
-var ip string
+var host string
 var port string
 var mode string
 var location string
 
 func init() {
-	flag.StringVar(&ip, "ip", "127.0.0.1", "Defines host ip")
+	flag.StringVar(&host, "host", "127.0.0.1", "Defines host ip")
+	flag.StringVar(&host, "h", "127.0.0.1", "Defines host ip")
 	flag.StringVar(&port, "port", "9090", "Defines host port")
 	flag.StringVar(&port, "p", "9090", "Defines host port")
 	flag.StringVar(&mode, "mode", "memory", "Defines storage location")
@@ -55,7 +56,7 @@ func main() {
 	storageInit()
 	cleanup()
 
-	l, err := net.Listen("tcp", ip+":"+port)
+	l, err := net.Listen("tcp", host+":"+port)
 
 	if err != nil {
 		log.Error.Panicln(err.Error())
